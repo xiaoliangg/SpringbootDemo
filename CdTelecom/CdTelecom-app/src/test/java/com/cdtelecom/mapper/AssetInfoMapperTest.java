66b9f5ae81ad207cdb36177410f4911b209e79c9
yuliang <yuliang@linksfield.net>
2020-03-26 15:21:02 +0800

    11、springboot 多profile以支持多种线上环境;junit 单元测试 支持多种Profile

--- CdTelecom/CdTelecom-app/src/test/java/com/cdtelecom/mapper/AssetInfoMapperTest.java
+++ CdTelecom/CdTelecom-app/src/test/java/com/cdtelecom/mapper/AssetInfoMapperTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 public class AssetInfoMapperTest {
 
     @Autowired
